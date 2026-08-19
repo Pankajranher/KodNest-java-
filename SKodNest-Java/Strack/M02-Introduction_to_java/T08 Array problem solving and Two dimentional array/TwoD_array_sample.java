609439c530c2aab22b6666429fc3c2aba22fcdd2
Pankaj Ranher <pankajranher@gmail.com>
2026-08-19 21:04:42 +0530

    Added T08 Array problem solving and Two dimentional array practice programs

---
+++ SKodNest-Java/Strack/M02-Introduction_to_java/T08 Array problem solving and Two dimentional array/TwoD_array_sample.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class TwoD_array_sample {
+    public static void main(String[] args) {
+        int a[][] = new int[3][5];
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the values of array");
+        for (int i = 0; i <= 2; i++) {
+            for (int j = 0; j <= 4; j++) {
+                a[i][j] = sc.nextInt();
+            }
+        }
+        for (int i = 0; i <= 2; i++) {
+            for (int j = 0; j <= 4; j++) {
+                System.out.print(a[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+}
