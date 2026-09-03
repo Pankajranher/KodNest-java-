b96e523f9019f05d1196147745d654ae83e1cfd0
Pankaj Ranher <pankajranher@gmail.com>
2026-09-03 22:30:02 +0530

    Added pending last program from T02 Searching and simple travers.

---
+++ SKodNest-Java/Strack/M03-Array-Problem-Solving/T02 Searching and simple state tracking/Find_first_non_repeting_value.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Find_first_non_repeting_value {
+    public static void main(String arg[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int result = -1;
+        for (int i = 0; i < n; i++) {
+            int count = 0;
+            for (int j = 0; j < n; j++) {
+                if (arr[i] == arr[j]) {
+                    count++;
+                }
+            }
+            if (count == 1) {
+                result = arr[i];
+                break;
+            }
+        }
+        System.out.println(result);
+
+    }
+
+}
